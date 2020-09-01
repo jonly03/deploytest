c3044e5dbbe067cd5ba2c04591052adc1282aac3
Nelly Sugu <jonly03@gmail.com>
2020-09-01 12:18:06 -0400

    changed port assignment

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ server.get("/", (req, res) => {
   res.send("Welcome back home");
 });
 
-server.listen(3000, () => {
+let PORT = process.env.PORT ? process.env.PORT : 3000;
+
+server.listen(PORT, () => {
   console.log("server listening");
 });
